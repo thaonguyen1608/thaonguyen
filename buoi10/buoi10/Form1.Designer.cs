201ddf6e943d3fa0463c85cacae8f8cbdcde4dcb
Huynh Thao Nguyen <nuyenml0984@gmail.com>
2023-11-22 14:41:10 +0700

    add new file minhchung and buoi10

---
+++ buoi10/buoi10/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace buoi10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCauHoi = new System.Windows.Forms.Label();
+            this.rbA = new System.Windows.Forms.RadioButton();
+            this.rbB = new System.Windows.Forms.RadioButton();
+            this.rbC = new System.Windows.Forms.RadioButton();
+            this.rbD = new System.Windows.Forms.RadioButton();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCauHoi
+            // 
+            this.lblCauHoi.AutoSize = true;
+            this.lblCauHoi.Location = new System.Drawing.Point(18, 24);
+            this.lblCauHoi.Name = "lblCauHoi";
+            this.lblCauHoi.Size = new System.Drawing.Size(43, 13);
+            this.lblCauHoi.TabIndex = 0;
+            this.lblCauHoi.Text = "Cau hoi";
+            // 
+            // rbA
+            // 
+            this.rbA.AutoSize = true;
+            this.rbA.Location = new System.Drawing.Point(15, 107);
+            this.rbA.Name = "rbA";
+            this.rbA.Size = new System.Drawing.Size(32, 17);
+            this.rbA.TabIndex = 1;
+            this.rbA.TabStop = true;
+            this.rbA.Text = "A";
+            this.rbA.UseVisualStyleBackColor = true;
+            // 
+            // rbB
+            // 
+            this.rbB.AutoSize = true;
+            this.rbB.Location = new System.Drawing.Point(96, 107);
+            this.rbB.Name = "rbB";
+            this.rbB.Size = new System.Drawing.Size(32, 17);
+            this.rbB.TabIndex = 2;
+            this.rbB.TabStop = true;
+            this.rbB.Text = "B";
+            this.rbB.UseVisualStyleBackColor = true;
+            // 
+            // rbC
+            // 
+            this.rbC.AutoSize = true;
+            this.rbC.Location = new System.Drawing.Point(168, 107);
+            this.rbC.Name = "rbC";
+            this.rbC.Size = new System.Drawing.Size(32, 17);
+            this.rbC.TabIndex = 3;
+            this.rbC.TabStop = true;
+            this.rbC.Text = "C";
+            this.rbC.UseVisualStyleBackColor = true;
+            // 
+            // rbD
+            // 
+            this.rbD.AutoSize = true;
+            this.rbD.Location = new System.Drawing.Point(239, 107);
+            this.rbD.Name = "rbD";
+            this.rbD.Size = new System.Drawing.Size(33, 17);
+            this.rbD.TabIndex = 4;
+            this.rbD.TabStop = true;
+            this.rbD.Text = "D";
+            this.rbD.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(190, 194);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.rbD);
+            this.Controls.Add(this.rbC);
+            this.Controls.Add(this.rbB);
+            this.Controls.Add(this.rbA);
+            this.Controls.Add(this.lblCauHoi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCauHoi;
+        private System.Windows.Forms.RadioButton rbA;
+        private System.Windows.Forms.RadioButton rbB;
+        private System.Windows.Forms.RadioButton rbC;
+        private System.Windows.Forms.RadioButton rbD;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
+
---
+++ buoi10/buoi10/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buoi10
+{
+    public partial class Form1 : Form
+    {
+        private List<CauHoi> danhSachCauHoi;
+        private int currentIndex;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            danhSachCauHoi = LayDanhSachCauHoiTuDatabase(); // Hàm này sẽ trả về danh sách câu hỏi từ SQL
+            currentIndex = 0;
+            HienThiCauHoi(currentIndex);
+        }
+
+        private List<CauHoi> LayDanhSachCauHoiTuDatabase()
+        {
+            List<CauHoi> danhSach = new List<CauHoi>();
+
+            string connectionString = "Data Source=A209PC03;Initial Catalog=master;Integrated Security=True";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "SELECT * FROM CauHoi";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string noiDung = reader["NoiDung"].ToString();
+                                string dapAnA = reader["DapAnA"].ToString();
+                                string dapAnB = reader["DapAnB"].ToString();
+                                string dapAnC = reader["DapAnC"].ToString();
+                                string dapAnD = reader["DapAnD"].ToString();
+
+                                CauHoi cauHoi = new CauHoi(noiDung, dapAnA, dapAnB, dapAnC, dapAnD);
+                                danhSach.Add(cauHoi);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+               
+            }
+
+            return danhSach;
+        }
+
+
+        private void HienThiCauHoi(int index)
+        {
+            if (index >= 0 && index < danhSachCauHoi.Count)
+            {
+                lblCauHoi.Text = danhSachCauHoi[index].NoiDung;
+                rbA.Text = danhSachCauHoi[index].DapAnA;
+                rbB.Text = danhSachCauHoi[index].DapAnB;
+                rbC.Text = danhSachCauHoi[index].DapAnC;
+                rbD.Text = danhSachCauHoi[index].DapAnD;
+            }
+        }
+
+       
+        private void btnNext_Click_1(object sender, EventArgs e)
+        {
+            currentIndex++;
+            HienThiCauHoi(currentIndex);
+        }
+    }
+
+    public class CauHoi
+    {
+        public string NoiDung { get; set; }
+        public string DapAnA { get; set; }
+        public string DapAnB { get; set; }
+        public string DapAnC { get; set; }
+        public string DapAnD { get; set; }
+
+        public CauHoi(string noiDung, string dapAnA, string dapAnB, string dapAnC, string dapAnD)
+        {
+            NoiDung = noiDung;
+            DapAnA = dapAnA;
+            DapAnB = dapAnB;
+            DapAnC = dapAnC;
+            DapAnD = dapAnD;
+        }
+    }
+}
